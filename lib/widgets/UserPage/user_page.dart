e6b39d3278e5b8c76be5297d447c139e7a433b47
vas.claudio15 <vas.claudio15@gmail.com>
2024-08-20 02:02:34 +0000

    inicializacion rutas

---
+++ lib/widgets/UserPage/user_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatelessWidget {
+  const UserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+        centerTitle: true,
+      ),
+      body: Center(child: Text('Hello world')),
+    );
+  }
+}
+
+
+
